7e78bc30a25a2be7ff5db5c29a8b1c92a3d8df62
Kamil Krzempek <kamilkrzempek99@gmail.com>
2022-03-19 10:42:11 +0100

    add basic search view

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { APP_ROUTES } from "./commons/routes";
+import { muiTheme } from "./commons/muiTheme";
 import { Search } from "./views";
 
 export const Root = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={APP_ROUTES.SEARCH} element={<Search />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path={APP_ROUTES.SEARCH} element={<Search />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
--- frontend/src/views/Search/Search.tsx
+++ frontend/src/views/Search/Search.tsx
@@ -1,12 +1,125 @@
-import React from "react";
-import { Container, Grid } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  IconButton,
+  Collapse,
+} from "@mui/material";
+import { SubmitHandler, useForm } from "react-hook-form";
+import {
+  FolderOpen as FolderIcon,
+  FilterList as FilterListIcon,
+  Search as SearchIcon,
+} from "@mui/icons-material";
+import cx from "classnames";
+import styles from "./Search.module.scss";
+
+type FormData = {
+  searchQuery: string;
+  rootDirectory: string;
+};
 
 export const Search = () => {
+  const [filtersExpanded, setFiltersExpanded] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container>
-      <Grid>
-        <h2>Ultimate Search Engine</h2>
-      </Grid>
+      <h2>Ultimate Search Engine</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Paper
+          elevation={8}
+          sx={{ p: 2 }}
+          className={cx({
+            [styles.borderRadiusTopOnly]: filtersExpanded,
+          })}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                id="search-query"
+                label="Search query"
+                variant="outlined"
+                fullWidth
+                error={!!errors.searchQuery}
+                helperText={errors.searchQuery?.message ?? "Phrase in Polish"}
+                {...register("searchQuery", {
+                  required: "Field must not be empty",
+                })}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <TextField
+                id="root-catalog"
+                label="Root catalog"
+                variant="outlined"
+                fullWidth
+                error={!!errors.rootDirectory}
+                helperText={
+                  errors.rootDirectory?.message ?? 'E.g. "/home/user/"'
+                }
+                InputProps={{
+                  endAdornment: <FolderIcon />,
+                }}
+                {...register("rootDirectory", {
+                  required: "Field must not be empty",
+                })}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={1}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton
+                size="large"
+                onClick={() => setFiltersExpanded(!filtersExpanded)}
+              >
+                <FilterListIcon fontSize="inherit" />
+              </IconButton>
+            </Grid>
+            <Grid
+              item
+              xs={1}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <IconButton size="large" type="submit">
+                <SearchIcon fontSize="inherit" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Paper>
+        <Collapse in={filtersExpanded}>
+          <Paper
+            elevation={12}
+            sx={{ p: 2 }}
+            className={cx({
+              [styles.borderRadiusBottomOnly]: filtersExpanded,
+            })}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                Advanced filters coming soon
+              </Grid>
+            </Grid>
+          </Paper>
+        </Collapse>
+      </form>
     </Container>
   );
 };
