e7245beca9551ca5c9fb17c71af2063727659790
Pan-Maciek <koziejka.com@gmail.com>
2022-03-19 14:00:41 +0100

    ASP configuration with search controller

---
+++ text-search/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿namespace TextSearch.Controllers; 
+
+[ApiController]
+[Route("/search")]
+public class SearchController : ControllerBase {
+
+    private readonly ILogger<SearchController> _logger;
+
+    public SearchController(ILogger<SearchController> logger) {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public string Get(string path, string query, string fileSearchPattern) {
+        _logger.LogInformation("{Path} {SearchPattern} \"{Query}\"", path, fileSearchPattern, query);
+        return "";
+    }
+}
