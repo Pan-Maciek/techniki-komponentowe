bff7941457c6e90e83c16f7b5fc521130947a728
Kamil Krzempek <kamilkrzempek99@gmail.com>
2022-03-19 08:16:08 +0100

    initial frontend setupp

---
+++ frontend/src/views/Search/Search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Grid } from "@mui/material";
+
+export const Search = () => {
+  return (
+    <Container>
+      <Grid>
+        <h2>Ultimate Search Engine</h2>
+      </Grid>
+    </Container>
+  );
+};
