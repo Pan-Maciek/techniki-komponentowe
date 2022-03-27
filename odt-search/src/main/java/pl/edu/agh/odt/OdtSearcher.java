11a25875c25efcd9d62a53901f05faf3f857c558
enkelian <aniano13@gmail.com>
2022-03-27 18:14:02 +0200

    create odt search POC

---
+++ odt-search/src/main/java/pl/edu/agh/odt/OdtSearcher.java
@@ -0,0 +1,27 @@
+package pl.edu.agh.odt;
+
+
+import org.odftoolkit.odfdom.doc.OdfTextDocument;
+import org.odftoolkit.odfdom.incubator.search.TextNavigation;
+import org.odftoolkit.odfdom.incubator.search.TextSelection;
+
+public class OdtSearcher {
+
+    public static void main(String[] args) throws Exception
+    {
+        OdfTextDocument textdoc = OdfTextDocument.loadDocument("./odt-search/src/main/resources/bells.odt");
+
+        TextNavigation search1;
+
+        search1 = new TextNavigation("bells",textdoc);
+
+        while (search1.hasNext()) {
+
+            TextSelection item1 = (TextSelection) search1.getCurrentItem();
+
+            System.out.println(item1);
+
+        }
+    }
+
+}
