665c1c7b3c23c46341840dbc5eb267dff393285f
Maciej Kozieja <kozieja.maciej@gmail.com>
2022-04-03 17:57:42 +0200

    added basic error handling

--- text-search/Controllers/SearchController.cs
+++ text-search/Controllers/SearchController.cs
@@ -13,9 +13,39 @@ public class SearchController : ControllerBase {
     }
 
     [HttpGet]
-    public List<FileSearchResult> Get(string phrase, string rootPath) {
+    public IActionResult Get(string phrase, string rootPath) {
         _logger.LogInformation("Searching in {Path} for *.txt | *.md files containing '{Phrase}'", rootPath, phrase);
 
-        return _fileFinder.FindInDirectory(rootPath, phrase);
+        try
+        {
+            var results =  _fileFinder.FindInDirectory(rootPath, phrase);
+            return Ok(new SearchResponse(results));
+        }
+        catch (Exception e)
+        {
+            _logger.LogInformation("Search in {Path} failed with: {Error}", rootPath, e.Message);
+            return Ok(new SearchResponse(e));
+        }
+    }
+}
+
+public class SearchResponse
+{
+    public string Status { get; }
+    public List<FileSearchResult> Results { get; } 
+    public List<string> Errors { get; }
+
+    public SearchResponse(List<FileSearchResult> results)
+    {
+        Status = "ok";
+        Results = results;
+        Errors = new List<string>();
+    }
+
+    public SearchResponse(Exception exception)
+    {
+        Status = "error";
+        Results = new List<FileSearchResult>();
+        Errors = new List<string> {exception.Message};
     }
 }
