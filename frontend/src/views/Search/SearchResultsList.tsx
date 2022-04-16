359bde9a119a5112725b2caa08963cf877fb487f
Kamil Krzempek <kamilkrzempek99@gmail.com>
2022-04-16 19:04:56 +0200

    add tree view

--- frontend/src/views/Search/SearchResultsList.tsx
+++ frontend/src/views/Search/SearchResultsList.tsx
@@ -1,9 +1,17 @@
 import React, { Fragment } from "react";
 import { SearchState } from "./Search";
 import { Skeleton } from "@mui/material";
+import { TreeView, TreeItem } from "@mui/lab";
+import {
+  ExpandMore as ExpandMoreIcon,
+  ChevronRight as ChevronRightIcon,
+} from "@mui/icons-material";
+import set from "lodash/set";
+
 import { SearchResponse } from "../../commons/types";
 import { SearchResult } from "./SearchResult";
 import { SearchError } from "./SearchError";
+import styles from "./Search.module.scss";
 
 type SearchResultsProps = {
   state: SearchState;
@@ -11,21 +19,69 @@ type SearchResultsProps = {
 
 export const SearchResultsList: React.VFC<SearchResultsProps> = ({ state }) => {
   if (state.status === "SUCCESS") {
+    const tree = {};
+
+    Object.entries(state.results).forEach(([service, { phrase, results }]) =>
+      results.forEach(({ filePath, matches }) => {
+        const item = {
+          phrase,
+          filePath,
+          service: service as keyof SearchResponse,
+          matches,
+        };
+
+        set(
+          tree,
+          filePath.replace(state.rootPath, "").split("/").splice(1),
+          item
+        );
+      })
+    );
+
+    const renderTree = (node) => (
+      <Fragment>
+        {Object.keys(node).map((key) =>
+          !key.includes(".") ? (
+            <TreeItem
+              nodeId={key}
+              label={key}
+              classes={{
+                content: styles.treeItemContent,
+                label: styles.treeItemLabel,
+              }}
+            >
+              {renderTree(node[key])}
+            </TreeItem>
+          ) : null
+        )}
+        {Object.keys(node).map((key) =>
+          key.includes(".") ? <SearchResult {...node[key]} /> : null
+        )}
+      </Fragment>
+    );
+
     return (
       <Fragment>
         {Object.entries(state.results).map(([service, { errors }]) => (
           <SearchError service={service} errors={errors} />
         ))}
-        {Object.entries(state.results).map(([service, { phrase, results }]) =>
-          results.map((result) => (
-            <SearchResult
-              phrase={phrase}
-              filePath={result.filePath}
-              service={service as keyof SearchResponse}
-              matches={result.matches}
-            />
-          ))
-        )}
+        <TreeView
+          defaultCollapseIcon={<ExpandMoreIcon />}
+          defaultExpandIcon={<ChevronRightIcon />}
+          defaultExpanded={[state.rootPath]}
+          sx={{ my: 4 }}
+        >
+          <TreeItem
+            nodeId={state.rootPath}
+            label={state.rootPath}
+            classes={{
+              content: styles.treeItemContent,
+              label: styles.treeItemLabel,
+            }}
+          >
+            {renderTree(tree)}
+          </TreeItem>
+        </TreeView>
       </Fragment>
     );
   }
