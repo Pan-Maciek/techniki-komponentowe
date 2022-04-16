67fa5df657a6b8b1aebe3fdfc53fa5fbe620e05d
Pan-Maciek <kozieja.maciej@gmail.com>
2022-04-17 01:29:31 +0200

    translation service implementation and usage

--- backend/src/main/kotlin/pl/edu/agh/backend/BackendApplication.kt
+++ backend/src/main/kotlin/pl/edu/agh/backend/BackendApplication.kt
@@ -1,10 +1,23 @@
 package pl.edu.agh.backend
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.InjectionPoint
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+
 
 @SpringBootApplication
-class BackendApplication
+class BackendApplication {
+	@Bean
+	fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder.build()
+
+	@Bean
+	fun logger(injectionPoint: InjectionPoint): Logger = LoggerFactory.getLogger( injectionPoint.methodParameter?.containingClass ?: injectionPoint.field?.declaringClass) }
+
 
 fun main(args: Array<String>) {
 	runApplication<BackendApplication>(*args)
