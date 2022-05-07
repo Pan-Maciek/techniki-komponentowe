b8d38eea0d9709a1051d674adb54725e7ee2e9a2
Michał Węgrzyn <44765778+mwegrzyn2311@users.noreply.github.com>
2022-05-07 21:45:41 +0200

    Audio-search service (#32)
    * audio-search service impl
    
    * minor fixes for audio-search
    
    * Handle multiple phrases
    
    * adjust naming
    
    * Add polish samples
    
    * Change array handling

---
+++ audio-search/src/result/__init__.py
@@ -0,0 +1,3 @@
+from .matches import Matches
+from .file_search_result import FileSearchResult
+from .request_result import RequestResult
--- backend/src/main/kotlin/pl/edu/agh/backend/search/ServiceMap.kt
+++ backend/src/main/kotlin/pl/edu/agh/backend/search/ServiceMap.kt
@@ -5,7 +5,8 @@ class ServiceMap {
         private val servicesNames : Map<String, String> = mapOf(
                 "text-search" to "80",
                 "odt-search" to "8182",
-                "pdf-search" to "8183"
+                "pdf-search" to "8183",
+                "audio-search" to "8184"
         )
 
         fun filterServices(enabledFormats: List<String>) : Map<String, String> = servicesNames.filter { it.key in enabledFormats }
