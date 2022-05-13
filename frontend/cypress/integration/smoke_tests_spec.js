cf260281480f10fd9f6cc7ee9dd622f6b7e93e77
Kamil Krzempek <kamilkrzempek99@gmail.com>
2022-05-13 16:52:37 +0200

    add more cypress tests

--- frontend/cypress/integration/smoke_tests_spec.js
+++ frontend/cypress/integration/smoke_tests_spec.js
@@ -1,9 +1,21 @@
 describe("Smoke tests", () => {
+  const checkOnly = (option) => {
+    const options = ["Plain text", "ODT", "PDF", "Audio files"];
+    options
+      .filter((opt) => opt !== option)
+      .forEach((opt) => cy.findByRole("checkbox", { name: opt }).click());
+  };
+
   it("Finds plain text files", () => {
     cy.visit("/");
     cy.findByRole("textbox", { name: "Search query" }).type("art");
     cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
+    cy.findByRole("button", { name: "Show filters" }).click();
+    checkOnly("Plain text");
+
     cy.findByRole("button", { name: "Search" }).click();
+
     cy.findByText("/app/files/sample.md").should("exist");
     cy.findByText("/app/files/sample.txt").should("exist");
   });
@@ -12,7 +24,12 @@ describe("Smoke tests", () => {
     cy.visit("/");
     cy.findByRole("textbox", { name: "Search query" }).type("bells");
     cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
+    cy.findByRole("button", { name: "Show filters" }).click();
+    checkOnly("ODT");
+
     cy.findByRole("button", { name: "Search" }).click();
+
     cy.findByText("/app/files/bells.odt").should("exist");
   });
 
@@ -20,20 +37,55 @@ describe("Smoke tests", () => {
     cy.visit("/");
     cy.findByRole("textbox", { name: "Search query" }).type("bomb");
     cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
+    cy.findByRole("button", { name: "Show filters" }).click();
+    checkOnly("PDF");
+
     cy.findByRole("button", { name: "Search" }).click();
+
     cy.findByText("/app/files/ktane-manual.pdf").should("exist");
   });
 
-  it("Finds only specified formats", () => {
+  it("Finds audio files", () => {
     cy.visit("/");
-    cy.findByRole("textbox", { name: "Search query" }).type("art");
+    cy.findByRole("textbox", { name: "Search query" }).type("kolor");
     cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
     cy.findByRole("button", { name: "Show filters" }).click();
-    cy.findByRole("checkbox", { name: "ODT" }).click();
-    cy.findByRole("checkbox", { name: "PDF" }).click();
+    checkOnly("Audio files");
+
     cy.findByRole("button", { name: "Search" }).click();
-    cy.findByText("/app/files/sample.md").should("exist");
-    cy.findByText("/app/files/sample.txt").should("exist");
-    cy.findByText("/app/files/bells.odt").should("not.exist");
+
+    cy.findByText("/app/files/kret_d044.wav").should("exist");
+  });
+
+  it("Finds files in subfolders", () => {
+    cy.visit("/");
+    cy.findByRole("textbox", { name: "Search query" }).type("colour");
+    cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
+    cy.findByRole("button", { name: "Show filters" }).click();
+    checkOnly("PDF");
+
+    cy.findByRole("button", { name: "Search" }).click();
+
+    cy.findByRole("treeitem", { name: "subfolder" }).click();
+    cy.findByText("/app/files/subfolder/death_of_all_colours.pdf").should(
+      "exist"
+    );
+  });
+
+  it("Finds translated phrase", () => {
+    cy.visit("/");
+    cy.findByRole("textbox", { name: "Search query" }).type("serce");
+    cy.findByRole("textbox", { name: "Root catalog" }).type("/app/files");
+
+    cy.findByRole("button", { name: "Show filters" }).click();
+    checkOnly("ODT");
+
+    cy.findByRole("button", { name: "Search" }).click();
+
+    cy.findByText("/app/files/bells.odt").should("exist");
+    cy.findByText("/app/files/mein_herz_brennt.odt").should("exist");
   });
 });
