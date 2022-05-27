a62e74915dd989a435c171af9d1fea4a74434d95
deszczowaa <deszczowaa@gmail.com>
2022-05-27 12:52:23 +0200

    test

--- backend/src/main/kotlin/pl/edu/agh/backend/search/AdditionalInfo.kt
+++ backend/src/main/kotlin/pl/edu/agh/backend/search/AdditionalInfo.kt
@@ -1,3 +1,4 @@
 package pl.edu.agh.backend.search
 
-class AdditionalInfo(var enabledFormats : List<String> = emptyList(), var lang: List<String> = emptyList())
+class AdditionalInfo(var enabledFormats : List<String> = emptyList(), var lang: List<String> = emptyList(),
+                     var forms : List<String> = emptyList())
